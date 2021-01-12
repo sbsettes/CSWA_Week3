50c72742c1f96d12b6c1d06743657f31cf7d17db
sbsettes <bensalem.salma24@gmail.com>
2021-01-12 18:40:57 +0100

    Initial Project

---
+++ tests/test-helper.js
@@ -0,0 +1,12 @@
+import Application from 'week03/app';
+import config from 'week03/config/environment';
+import * as QUnit from 'qunit';
+import { setApplication } from '@ember/test-helpers';
+import { setup } from 'qunit-dom';
+import { start } from 'ember-qunit';
+
+setApplication(Application.create(config.APP));
+
+setup(QUnit.assert);
+
+start();
